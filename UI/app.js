a6fd9c1f4a2df8b54b40309cf9bf1dc7fd41b553
Rajnikanth Kulkarni <Rajnikanthklk@gmail.com>
2021-01-18 19:25:39 +0530

    adding files

---
+++ UI/app.js
@@ -0,0 +1,101 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
+        $.post(url, {
+            image_data: file.dataURL
+        },function(data, status) {
+            /* 
+            Below is a sample response if you have two faces in an image lets say virat and roger together.
+            Most of the time if there is one person in the image you will get only one element in below array
+            data = [
+                {
+                    class: "Alia_Bhatt",
+                    class_probability: [88.59, 8.09, 1.07, 1.83, 0.42],
+                    class_dictionary: {
+                        Alia_Bhatt: 0,
+                        Deepika_Padukone: 1,
+                        Hrithik_Roshan: 2,
+                        Shahrukh_Khan: 3,
+                        Yash: 4
+                    }
+                },
+                {
+                    class: "Hrithik_Roshan",
+                    class_probability: [7.02, 23.7, 52.00, 6.1, 1.62],
+                    class_dictionary: {
+                        Alia_Bhatt: 0,
+                        Deepika_Padukone: 1,
+                        Hrithik_Roshan: 2,
+                        Shahrukh_Khan: 3,
+                        Yash: 4
+                    }
+                }
+            ]
+            */
+            console.log(data);
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            let players = ["Alia_Bhatt", "Deepika_Padukone", "Hrithik_Roshan", "Shahrukh_Khan", "Yash",];
+            
+            let match = null;
+            let bestScore = -1;
+            for (let i=0;i<data.length;++i) {
+                let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                if(maxScoreForThisClass>bestScore) {
+                    match = data[i];
+                    bestScore = maxScoreForThisClass;
+                }
+            }
+            if (match) {
+                $("#error").hide();
+                $("#resultHolder").show();
+                $("#divClassTable").show();
+                $("#resultHolder").html($(`[data-player="${match.class}"`).html());
+                let classDictionary = match.class_dictionary;
+                for(let personName in classDictionary) {
+                    let index = classDictionary[personName];
+                    let proabilityScore = match.class_probability[index];
+                    let elementName = "#score_" + personName;
+                    $(elementName).html(proabilityScore);
+                }
+            }
+            // dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
---
+++ readme.md
@@ -0,0 +1,24 @@
+![](ui_snapshot.jpg)
+
+In this data science and machine learning project, we classify Celiberity personalities. We restrict classification to only 5 people,
+1) Alia Bhatt
+2) Deepika Padukone
+3) Hrithik Roshan
+4) Shahrukh Khan
+5) Yash
+
+Here is the folder structure,
+* UI : This contains ui website code 
+* server: Python flask server
+* model: Contains python notebook for model building
+* images_dataset: Dataset used for our model training
+
+Technologies used in this project,
+1. Python
+2. Numpy and OpenCV for data cleaning
+3. Matplotlib & Seaborn for data visualization
+4. Sklearn for model building
+5. Jupyter notebook, visual studio code and pycharm as IDE
+6. Python flask for http server
+7. HTML/CSS/Javascript for UI
+
